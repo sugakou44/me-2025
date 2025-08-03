9fc40635729d5702d40f1e0978359d0df9c94428
Sugata Koufuku <sugakou44@gmail.com>
2025-08-04 00:33:20 +0700

    feat: removed delay before display

--- src/components/NameCard/NameCard.svelte
+++ src/components/NameCard/NameCard.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  import { tick, untrack } from 'svelte'
+  import { untrack } from 'svelte'
   import { backOut, cubicOut } from 'svelte/easing'
   import { Tween } from 'svelte/motion'
   import { fly, scale } from 'svelte/transition'
@@ -17,7 +17,7 @@
   let isOpen = $state(false)
 
   const isIn = $derived(appState.shouldShowHeroDecorator || isOpen)
-  const initialIsIn = $derived(untrack(() => isIn))
+  const initialIsIn = true
 
   const deviceType = useDeviceType()
 
@@ -58,64 +58,62 @@
   })
 </script>
 
-{#await tick() then}
-  <div
-    in:fly={{
-      y: initialIsIn ? 0 : -100,
-      opacity: 0.01,
-      duration: defaultDuration() ?? 0,
-      easing: backOut,
-    }}
-    class="pointer-events-none fixed inset-4 z-50 will-change-transform md:inset-6"
-  >
-    {#if appState.shouldShowHeroDecorator}
-      <div
-        in:scale={{
-          opacity: 0.01,
-          duration: DURATION_NORMAL,
-          easing: backOut,
-        }}
-        out:scale={{
-          opacity: 0.01,
-          duration: DURATION_SLOW,
-          easing: cubicOut,
-        }}
-        class="absolute inset-0 bg-background/80 will-change-transform"
-      ></div>
-    {/if}
+<div
+  in:fly={{
+    y: initialIsIn ? 0 : -100,
+    opacity: 0.01,
+    duration: defaultDuration() ?? 0,
+    easing: backOut,
+  }}
+  class="pointer-events-none fixed inset-4 z-50 will-change-transform md:inset-6"
+>
+  {#if appState.shouldShowHeroDecorator}
     <div
-      style:transform={contentTransform}
-      class="section pointer-events-auto absolute top-[45%] left-[50%] aspect-[1/1.65] w-full will-change-transform transform-3d md:aspect-[1.65]"
-    >
-      <Content
-        bind:isOpen
-        forceOpen={appState.shouldShowHeroDecorator}
-        {initialIsIn}
-      />
-      {#if !isIn}
-        <div class="absolute inset-1 -translate-z-1">
+      in:scale={{
+        opacity: 0.01,
+        duration: DURATION_NORMAL,
+        easing: backOut,
+      }}
+      out:scale={{
+        opacity: 0.01,
+        duration: DURATION_SLOW,
+        easing: cubicOut,
+      }}
+      class="absolute inset-0 bg-background/80 will-change-transform"
+    ></div>
+  {/if}
+  <div
+    style:transform={contentTransform}
+    class="section pointer-events-auto absolute top-[45%] left-[50%] aspect-[1/1.65] w-full will-change-transform transform-3d md:aspect-[1.65]"
+  >
+    <Content
+      bind:isOpen
+      forceOpen={appState.shouldShowHeroDecorator}
+      {initialIsIn}
+    />
+    {#if !isIn}
+      <div class="absolute inset-1 -translate-z-1">
+        <div
+          class="absolute right-0 bottom-0 !h-[96px] w-full -scale-x-100 md:top-[50%] md:right-12 md:w-[calc(100%/1.65)] md:translate-x-[50%] md:translate-y-[-50%] md:-rotate-90"
+        >
           <div
-            class="absolute right-0 bottom-0 !h-[96px] w-full -scale-x-100 md:top-[50%] md:right-12 md:w-[calc(100%/1.65)] md:translate-x-[50%] md:translate-y-[-50%] md:-rotate-90"
+            class="halftone-stripe absolute inset-4 bottom-0 rounded-t-none rounded-b-lg text-primary"
+          ></div>
+          <Button
+            variant="puller"
+            class="!h-full w-full rounded-t-none rounded-b-xl bg-transparent text-2xl md:text-3xl [&:hover>span]:shadow-lg"
+            onclick={() => {
+              isOpen = true
+            }}
           >
-            <div
-              class="halftone-stripe absolute inset-4 bottom-0 rounded-t-none rounded-b-lg text-primary"
-            ></div>
-            <Button
-              variant="puller"
-              class="!h-full w-full rounded-t-none rounded-b-xl bg-transparent text-2xl md:text-3xl [&:hover>span]:shadow-lg"
-              onclick={() => {
-                isOpen = true
-              }}
+            <span
+              class="pointer-events-none rounded-full bg-background px-10 py-5 text-primary-foreground"
             >
-              <span
-                class="pointer-events-none rounded-full bg-background px-10 py-5 text-primary-foreground"
-              >
-                CONTACT
-              </span>
-            </Button>
-          </div>
+              CONTACT
+            </span>
+          </Button>
         </div>
-      {/if}
-    </div>
+      </div>
+    {/if}
   </div>
-{/await}
+</div>
