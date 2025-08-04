63cb33d0ae3e0f2bce33a3737e47e52e599f330b
Sugata Koufuku <sugakou44@gmail.com>
2025-08-04 23:26:38 +0700

    chore: updated site title

--- src/lib/constants/seo.ts
+++ src/lib/constants/seo.ts
@@ -1,7 +1,7 @@
-export const DEFAULT_TITLE = 'PAAN'
+export const DEFAULT_TITLE = 'Saran「PAAN」Angsuwan'
 export const DEFAULT_DESCRIPTION =
   'Front-end developer based in Bangkok, Thailand'
 
 export function getTitle(title: string) {
-  return `${title} | ${DEFAULT_TITLE}`
+  return `${title} | PAAN`
 }
