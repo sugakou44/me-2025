697198ae5c98606e7b96cc88c5fe1f859afc6f5e
Sugata Koufuku <sugakou44@gmail.com>
2025-08-29 06:12:39 +0700

    feat: removed some animation

--- src/modules/main/pages/HomePage/sections/About/About.svelte
+++ src/modules/main/pages/HomePage/sections/About/About.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-  import { eases } from 'animejs'
   import { fade } from 'svelte/transition'
 
   import { DURATION_FAST } from '@/lib/animations/constants'
@@ -18,7 +17,6 @@
         duration: DURATION_FAST,
       }}
       class="page fixed top-[unset] bottom-8 left-1/2 max-w-xl -translate-x-1/2 -translate-y-1/2 will-change-transform xl:left-1/4"
-      style:transform={`translateY(${100 * (1 - eases.outSine(homeState.aboutScrollProgress)) * 1.25}%)`}
     >
       <Paragraph class="text-primary-foreground">
         Hello again
