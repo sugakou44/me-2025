44bad231028ea20737207fd74a83b0ef900dac9d
Sugata Koufuku <sugakou44@gmail.com>
2025-09-09 04:23:43 +0700

    feat: used useInView instead of scroll threshold

--- src/modules/main/pages/HomePage/sections/About/About.svelte
+++ src/modules/main/pages/HomePage/sections/About/About.svelte
@@ -1,25 +1,19 @@
 <script lang="ts">
   import { TECHSTACK_AS_ARRAY } from '@/lib/constants/content'
   import { squircleBackground } from '@/lib/svelte/backgroundSquircle.svelte'
+  import { useInView } from '@/lib/svelte/intersectionObserver.svelte'
   import HighlightText from '@/modules/main/components/Text/HighlightText.svelte'
   import { homeState } from '@/modules/main/contexts/HomeState'
 
   import StackItem from './StackItem.svelte'
 
-  let isIn = $state(false)
-
-  $effect(() => {
-    if (isIn && homeState.aboutScrollProgress < 0.1) {
-      isIn = false
-    } else if (!isIn && homeState.aboutScrollProgress > 0.3) {
-      isIn = true
-    }
-  })
+  let [containerRef, isInView] = useInView({ once: true })
 </script>
 
 <section class="section min-h-screen" bind:this={homeState.aboutContainer}>
   <div
     class="p-8"
+    bind:this={containerRef.current}
     {@attach squircleBackground({
       cornerRadius: 64,
       class: 'fill-white/80 z-0',
@@ -27,9 +21,10 @@
   >
     <p class="relative p-8 text-center text-primary-foreground">
       I’m a frontend developer who
-      <HighlightText visible={isIn}>likes making things move</HighlightText> — buttons,
-      text, whole pages — anything that feels a little more alive than just sitting
-      there.
+      <HighlightText visible={isInView()}
+        >likes making things move</HighlightText
+      > — buttons, text, whole pages — anything that feels a little more alive than
+      just sitting there.
     </p>
     <p
       class="relative p-8 text-center text-primary-foreground transition-all duration-200"
@@ -39,7 +34,7 @@
     </p>
     <ul class="relative flex flex-wrap justify-center gap-4">
       {#each TECHSTACK_AS_ARRAY as { name, Icon } (name)}
-        <StackItem {name} {Icon} {isIn} />
+        <StackItem {name} {Icon} isIn={isInView()} />
       {/each}
     </ul>
   </div>
