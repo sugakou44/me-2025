d937c32e4b35ea0e8f9ea95d35c00d251ecea551
Sugata Koufuku <sugakou44@gmail.com>
2025-09-09 11:50:40 +0700

    feat: added padding for noscript content

--- src/components/Noscript/Noscript.svelte
+++ src/components/Noscript/Noscript.svelte
@@ -1,21 +1,23 @@
 <noscript>
   <div
-    class="fixed inset-0 z-[1000] flex flex-col items-center justify-center gap-8 bg-white"
+    class="fixed inset-0 z-[1000] flex h-full w-full flex-col items-center justify-center bg-white"
   >
-    <h1 class="font-handwritting-heading">
-      Hey! <span class="hand-waving inline-block">👋</span>
-    </h1>
-    <p class="text-center">
-      This site is best viewed with Javascript. If you are unable to turn on
-      Javascript,
-      <br />
-      please visit
-      <a
-        class="actionable text-primary-foreground underline transition-opacity duration-200"
-        href="/documents/resume"
-      >
-        resume page</a
-      >.
-    </p>
+    <div class="section flex flex-col items-center justify-center gap-8">
+      <h1 class="font-handwritting-heading">
+        Hey! <span class="hand-waving inline-block">👋</span>
+      </h1>
+      <p class="text-center">
+        This site is best viewed with Javascript. If you are unable to turn on
+        Javascript,
+        <br />
+        please visit
+        <a
+          class="actionable text-primary-foreground underline transition-opacity duration-200"
+          href="/documents/resume"
+        >
+          resume page</a
+        >.
+      </p>
+    </div>
   </div>
 </noscript>
