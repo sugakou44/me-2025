de5a04260483bd914737ab495207af67c2e9349a
Sugata Koufuku <sugakou44@gmail.com>
2025-09-10 07:21:36 +0700

    fix: accessibility warning

--- src/modules/main/pages/HomePage/sections/Hero/Hero.svelte
+++ src/modules/main/pages/HomePage/sections/Hero/Hero.svelte
@@ -1,4 +1,4 @@
-<h1 class="fixed -top-[4em] left-1/2 text-white/5">Hi, I'm PAAN</h1>
-<h4 class="fixed -top-[2em] left-1/2 text-white/5">
+<h1 class="fixed -top-[4em] left-1/2 text-[1px] text-white/5">Hi, I'm PAAN</h1>
+<h2 class="fixed -top-[2em] left-1/2 text-[1px] text-white/5">
   Front-end developer based in Bangkok, Thailand
-</h4>
+</h2>
