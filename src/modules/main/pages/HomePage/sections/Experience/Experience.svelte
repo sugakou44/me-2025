f414dc2e16c417f970ff25f9c58889a84cfd81c6
Sugata Koufuku <sugakou44@gmail.com>
2025-09-22 16:27:33 +0700

    revert: updated text

--- src/modules/main/pages/HomePage/sections/Experience/Experience.svelte
+++ src/modules/main/pages/HomePage/sections/Experience/Experience.svelte
@@ -11,7 +11,7 @@
   style:--page-size="120svh"
   bind:this={homeState.experienceContainer}
 >
-  <SectionHeader>Jobs</SectionHeader>
+  <SectionHeader>Experience</SectionHeader>
   <div class="flex flex-col gap-80">
     {#each EXPERIENCE as item, index (item.company)}
       <ExperienceItem {...item} {index} />
